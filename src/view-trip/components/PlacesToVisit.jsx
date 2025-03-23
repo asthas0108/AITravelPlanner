67576f37ebc5bb84fc64330e5a9f0c4a859031ad
asthas0108 <asthas0108@gmail.com>
2025-03-23 11:57:24 +0530

    add files

--- src/view-trip/components/PlacesToVisit.jsx
+++ src/view-trip/components/PlacesToVisit.jsx
@@ -12,7 +12,7 @@ export default function PlacesToVisit({trip}) {
                     return (
                         <div className='mt-5'>
                             <h2 className='font-medium text-lg'>{item.day}</h2>
-                            <h2><span className='font-semibold'>Best time to visit: </span>{item.bestTime}</h2>
+                            {/* <h2><span className='font-semibold'>Best time to visit: </span>{item.bestTimeToVisit}</h2> */}
                             <div className='grid grid-cols-2 gap-5 mt-5'>
                                 {
                                     item.plan.map((place, index) => {
